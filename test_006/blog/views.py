6face19db5053a3216f07a1aa57edf600be9a842
Andrey-Marin <io_o.i@mail.ru>
2023-04-18 15:49:46 +0300

    Add forms in Blog app django

--- test_006/blog/views.py
+++ test_006/blog/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import ListView, DetailView
-from .models import Post
-# Create your views here.
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
+from .models import Post
+
+
+# Create your views here.
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -12,3 +16,19 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
 
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
